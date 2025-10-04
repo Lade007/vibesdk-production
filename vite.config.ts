e647313db1d904d58f202f405891bc98040cb01f
Omolade Ajibade <omolade.ajibade@softwarealliance.net>
2025-10-04 10:56:48 +0100

    bun lock deleted

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
 		exclude: ['format', 'editor.all'],
 		include: ['monaco-editor/esm/vs/editor/editor.api'],
 		force: true, // Force re-optimization on every start
+		rollupOptions: { ... }
 	},
 
 	// build: {
